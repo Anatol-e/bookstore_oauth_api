f42eb6ab9f3f345d8beff3bd592aa3aaa5970ca3
Anatol-e <bolshoymedvedtoliano@gmail.com>
2020-08-27 18:02:44 +0300

    Domain Driven Development and tests

---
+++ src/domain/access_token/access_token_test.go
@@ -0,0 +1,28 @@
+package access_token
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+const emptyString = ""
+
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValues(t, 24, expirationTime, "expiration time should be 24 hours")
+}
+
+func TestGetNewAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	assert.False(t, at.IsExpired(), "new access token should not be expired")
+	assert.EqualValues(t, at.AccessToken, emptyString, "new Access token should not have defined token")
+	assert.True(t, at.UserId == 0, "new access token should not have an associated user")
+}
+
+func TestAccessToken_IsExpired(t *testing.T) {
+	at := &AccessToken{}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
+
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token expired for tree hours")
+}
